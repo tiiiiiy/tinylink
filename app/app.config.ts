47feb1309c2368057c4279227a716918063a569a
tiiiiiy <97773341+tiiiiiy@users.noreply.github.com>
2026-06-12 11:50:47 +0800

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,13 +1,13 @@
 export default defineAppConfig({
-  title: 'Sink',
-  github: 'https://github.com/miantiao-me/sink',
-  coffee: 'https://sink.cool/coffee',
-  twitter: 'https://sink.cool/x',
-  telegram: 'https://sink.cool/telegram',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
-  image: 'https://sink.cool/banner.png',
+  title: 'TinyLink',
+  github: '',
+  coffee: '',
+  twitter: '',
+  telegram: '',
+  description: 'Link Shortener.',
+  image: '',
   previewTTL: 300, // 5 minutes
-  slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
+  slugRegex: /^[a-z0-9_!.~$'*()]+(?:-[a-z0-9_!.~$'*()]+)*$/i,
   reserveSlug: [
     'dashboard',
   ],
